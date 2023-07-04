abfdc806e306794cef62da3eb8287d1c7df89a98
unknown <mica.a.lugo12@gmail.com>
2023-07-03 22:57:34 -0300

    agregar la linea 32

--- ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
@@ -29,7 +29,9 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
+        """Mica"""
         pass
+
 
 
 if __name__ == "__main__":
